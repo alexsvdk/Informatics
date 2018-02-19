94dc4a5229f206d5709d1f69ced3975a87391b02
A1exS <alex.svdk@gmail.com>
2018-02-19 15:46:32 +0300

    tasklist

--- app/src/main/java/ru/temoteam/informatics/api/Requester.kt
+++ app/src/main/java/ru/temoteam/informatics/api/Requester.kt
@@ -3,14 +3,17 @@ package ru.temoteam.informatics.api
 import khttp.post
 import khttp.responses.Response
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import ru.temoteam.informatics.api.objects.*
+import java.util.*
 
 
 /**
  * Created by a1exS on 2/18/2018.
  */
 object Requester{
-    private val baseRequestUrl = "http://informatics.mccme.ru/"
+    private val baseRequestUrl = "http://informatics.mccme.ru"
     var account:Account? = null
 
     val mainPage:Response by lazy { baseReq("", emptyMap()) }
@@ -25,7 +28,7 @@ object Requester{
             baseReq(patch,null,null)
 
     fun login(username:String,password:String):Account? =
-            try { Account(username,password,post(baseRequestUrl +"login/index.php", emptyMap(), mapOf("username" to username,"password" to password)).headers["Set-Cookie"]!!) }
+            try { Account(username,password,post(baseRequestUrl +"/login/index.php", emptyMap(), mapOf("username" to username,"password" to password)).headers["Set-Cookie"]!!) }
             catch (e:Exception){ null }
 
     fun userData():Map<String,String>{
@@ -38,11 +41,31 @@ object Requester{
         val userdata = Jsoup.parse(baseReq(res["link"]!!).text).body().getElementById("content").getElementsByClass("userinfobox")[0].getElementsByClass("list")[0].getElementsByClass("info c1")
         res["city"] = userdata[0].text()
         res["school"] = userdata[1].text()
-        res["class"] = userdata[2].text()
+        res["classN"] = userdata[2].text()
         res["year"] = userdata[3].text()
-        res["courses"] = userdata[5].html()
+        res["rawCourses"] = userdata[5].html()
         res["lastLogin"] = userdata[6].text()
         res["id"] = res["link"]!!.substringAfter("?id=").substringBefore("&")
+        return res
+    }
+
+    fun courses():Map<String,Any>{
+        val res = HashMap<String,Any>()
+        val page = Jsoup.parse(mainPage.text).body()
+        page.getElementsByClass("tree")[0].select("div > ul > li").forEach {res[it.getElementsByClass("node category")[0].text()] = course(it.select("#${it.id()} > ul > li"))}
+        return res
+    }
+
+    private fun course(elements: Elements):Map<String,Any>{
+        val res = HashMap<String,Any>()
+        elements.forEach {
+            val nodes = it.getElementsByClass("node ")
+            if (nodes.isEmpty()){
+                val a = it.select("a")
+                res[a.text()] = a.attr("href")
+            }
+                res[nodes[0].text()] = course(it.select("#${it.id()} > ul > li"))
+        }
         return res
     }
 }
--- app/src/main/java/ru/temoteam/informatics/api/objects/Account.kt
+++ app/src/main/java/ru/temoteam/informatics/api/objects/Account.kt
@@ -1,5 +1,6 @@
 package ru.temoteam.informatics.api.objects
 
+import ru.temoteam.informatics.api.Requester
 import java.net.HttpCookie
 
 /**
@@ -9,6 +10,15 @@ class Account(val username: String,val password: String,val rawCookie:String) {
     val MoodleSessionTest = rawCookie.substringAfter("MoodleSessionTest=").substringBefore(";")
     val MoodleSession = rawCookie.substringAfter("MoodleSession=").substringBefore(";")
     val headers = mapOf("Cookie" to "MoodleSessionTest=$MoodleSessionTest; MoodleSession=$MoodleSession")
-
-
+    val data by lazy { Requester.account=this;Requester.userData() }
+    val firstName by data
+    val lastName by data
+    val link by data
+    val city by data
+    val school by data
+    val classN by data
+    val year by data
+    val rawCourses by data
+    val lastLogin by data
+    val id by data
 }
